6d754bffd38100c015b8ba53ddae35603f5da9eb
Aftab Ahmad <contactaftabahmad1@gmail.com>
2024-12-10 22:05:42 +0500

    Navbar Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import "./App.css";
+import Navbar from "./components/navbar";
+import Signup from "./pages/signup";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Navbar />
+      <Signup></Signup>
     </>
   );
 }
